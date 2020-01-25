5a74ac79e684c0c6594d1a6d2739d052e6e04756
maay21 <maay8536@colorado.edu>
2020-01-25 11:25:44 -0700

    Lab 01 and 02 PredPreyData.csv problem

---
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -0,0 +1,3 @@
+cut -f 2-4 -d, PredPreyData.csv > /Users/marinaayala/Lab01finalProblem.sh
+cut -f 1 PredPreyData.csv > /Users/marinaayala/Lab01finalProblem.sh
+tail PredPreyData.csv > /Users/marinaayala/Lab01finalProblem.sh
